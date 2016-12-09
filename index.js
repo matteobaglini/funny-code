13408fc19d41f76163970940f49492ced999cc0e
Matteo Baglini <matteo.baglini@gmail.com>
2016-12-09 19:33:30 +0100

    Start point

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const emptyList = []
+const oneItemList = [ "foo" ]
+const manyItemsList = [ "foo", "bar", "baz" ]
+
+
+const length = (list) => {
+  let count = 0
+  for(item in list)
+    count++
+  return count
+}
+
+
+
+
+
+
+
+
+
+
+
+// not interesting print utils
+(function () {
+
+  function print(what, tx, x) {
+    console.log(`=== ${what}`)
+    console.log(tx(x))
+    console.log()
+  }
+
+  console.log(`## length`)
+  print("empty", length, emptyList)
+  print("one", length, oneItemList)
+  print("many", length, manyItemsList)
+})()
